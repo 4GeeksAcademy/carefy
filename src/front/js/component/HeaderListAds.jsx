3bb6c5ffb7376dd84e8aaf783d82faf5a82c5255
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-08-08 13:50:59 +0000

    anadidos cambios en los estilos de la imagen del header

--- src/front/js/component/HeaderListAds.jsx
+++ src/front/js/component/HeaderListAds.jsx
@@ -11,7 +11,7 @@ const HeaderListAds = () => {
                 </div>
                 <div className="col-lg-6 img-container">
                     <img
-                        className="img-fluid imagen-encabezado"
+                        className="img-fluid imagen-encabezado img-fluid rounded"
                         alt="Un adulto y una persona mayor caminando y charlando en medio de la tranquilidad de la naturaleza"
                         title="Un adulto y una persona mayor caminando y charlando en medio de la tranquilidad de la naturaleza"
                         src="https://plus.unsplash.com/premium_photo-1681911657230-320f780ac474?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGhvbWJyZSUyMG1heW9yJTIwYW1pc3RhZHxlbnwwfHwwfHx8MA%3D%3D"
