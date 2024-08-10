5d962e5032b918b0e7f300a1053967aea319eabb
-TΦM- <137113975+JTomas88@users.noreply.github.com>
2024-08-10 16:10:29 +0000

    Se finaliza estructura del modal para añadir al usuario

--- src/front/js/component/AgregarFamiliar/AgregarFamiliar.jsx
+++ src/front/js/component/AgregarFamiliar/AgregarFamiliar.jsx
@@ -3,6 +3,37 @@ import { Link, useParams, useNavigate } from "react-router-dom";
 import style from "../AgregarFamiliar/agregarfamiliar.module.css"
 
 export const AgregarFamiliar = () => {
+    const [nacimiento, setNacimiento] = useState("");
+    const [edad, setEdad] = useState(0);
+
+    const calcularEdad = (fechaNacimiento) => {
+        const hoy = new Date()
+        const fechaNac = new Date(fechaNacimiento);
+        let edadCalculada = hoy.getFullYear() - fechaNac.getFullYear();
+        const mes = hoy.getMonth() - fechaNac.getMonth()
+
+        if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNac.getDate())) {
+            edadCalculada--;
+        }
+        return edadCalculada
+    }
+
+
+
+    /**
+     *Recoge la fecha que le hemos pasado a través del calendario.
+     *Lo pasa a la variable fechaNacimiento. 
+     *Guardamos esa fecha con el setNacimiento
+     *Guardamos el valor edad llamando a la función de arriba
+     * @param {e} Recibe el evento del input de la fecha
+     */
+    const handleBirthdayChange = (e) => {
+        const fechaNacimiento = e.target.value;
+        setNacimiento(fechaNacimiento);
+        setEdad(calcularEdad(fechaNacimiento));
+    };
+
+
 
     return (
         <form>
@@ -23,43 +54,101 @@ export const AgregarFamiliar = () => {
             </div>
 
             <div className="mb-3">
-                <label for="start">Start date:</label>
-                <input type="date" id="start" name="trip-start" value="2018-07-22" min="1900-01-01" max="2018-12-31" />
+                <div className="row">
+                    <div className="col-8">
+                        <label for="nacimiento">Fecha nacimiento:</label><br></br>
+                        <input onChange={handleBirthdayChange} type="date" id="nacimiento" name="nacimiento" value={nacimiento} />
+                    </div>
+                    <div className="col">
+                        <label for="birthday">Edad: </label><br></br>
+                        <label for="birthday">{edad} </label>
+
+                    </div>
+                </div>
+
             </div>
+
+
+            {/* <div className="mb-3">
+                <label htmlFor="telefono" className="form-label">¿Toma medicinas?</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Selecciona una opción:</option>
+                    <option value="1">Si</option>
+                    <option value="2">No</option>
+                </select>
+            </div> */}
 
             <div className="mb-3">
-                EDAD:
+                <label htmlFor="necesita" className="form-label">Selecciona las necesidades</label>
+                <div class="form-check">
+
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Supervisión y control de medicinas
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                    <label class="form-check-label" for="flexCheckChecked">
+                        Ayuda con comidas
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Ayuda con aseo
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                    <label class="form-check-label" for="flexCheckChecked">
+                        Ayuda con movilidad "leve"
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Ayuda con movilidad moderada y severa
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                    <label class="form-check-label" for="flexCheckChecked">
+                        Acompañamiento y entretenimiento
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                    <label class="form-check-label" for="flexCheckChecked">
+                        Paseos
+                    </label>
+                </div>
             </div>
-
-            <select class="form-select" aria-label="Default select example">
-                <label htmlFor="telefono" className="form-label">¿Toma medicinas?</label>
-                <option selected>Selecciona una opción:</option>
-                <option value="1">Si</option>
-                <option value="2">No</option>
-            </select>
-
-
-
-
-
 
 
             <div class="mb-3">
-                <label for="formFile" class="form-label">Default file input example</label>
-                <input class="form-control" type="file" id="formFile"/>
+            <label htmlFor="provincia" className="form-label">Provincia</label>
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Selecciona la provincia</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+            </div>
+
+
+            <div class="mb-3">
+                <label for="formFile" class="form-label">Puedes subir una foto</label>
+                <input class="form-control" type="file" id="formFile" />
             </div>
 
             <div className="mb-3">
-                <label htmlFor="nombre" className="form-label">Nombre</label>
-                <input type="text" className="form-control" id="nombre" />
+                <label htmlFor="observaciones" className="form-label">Otra información importante</label>
+                <textarea className="form-control" id="observaciones" placeholder="Puedes añadir cualquier información / necesidad relevante para el acompañante" />
             </div>
 
 
-            <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" claclassNamess="btn btn-primary">Submit</button>
+            <button type="submit" claclassNamess="btn btn-primary">Guardar usuario</button>
         </form>
 
     )
--- src/front/js/pages/PerfilUsuario/PerfilUsuario.jsx
+++ src/front/js/pages/PerfilUsuario/PerfilUsuario.jsx
@@ -3,6 +3,7 @@ import { Link, useParams, useNavigate } from "react-router-dom";
 import style from "../PerfilUsuario/perfilusuario.module.css";
 import nietaabuela from "../../../img/nietaabuela.jpg"
 import { FormularioFamiliar } from "../../component/FormularioFamiliar/FormularioFamiliar.jsx";
+import { AgregarFamiliar } from "../../component/AgregarFamiliar/AgregarFamiliar.jsx";
 
 export const PerfilUsuario = () => {
 
@@ -39,18 +40,14 @@ export const PerfilUsuario = () => {
 
                     {/* <!-- Modal --> */}
                     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
+                        <div class="modal-dialog modal-dialog-scrollable">
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h1 class="modal-title fs-5" id="exampleModalLabel">Agregar un familiar</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    ...
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                    <AgregarFamiliar />
                                 </div>
                             </div>
                         </div>
