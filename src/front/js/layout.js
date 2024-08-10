65b55c1c708b62fa90767109fd74034c47d65bf8
-TΦM- <137113975+JTomas88@users.noreply.github.com>
2024-08-10 08:52:26 +0000

    Se crea de nuevo la rama Login y se suben los cambios

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,7 +10,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
-
+import { Login } from "./pages/Login/Login.jsx"
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
@@ -29,6 +29,7 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
+                        <Route element={<Login />} path="/login" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
