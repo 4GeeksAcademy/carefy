ff9e7188b09e5a8801f5bc32750be656371d7462
-TΦM- <137113975+JTomas88@users.noreply.github.com>
2024-08-12 08:32:00 +0000

    modificacion

--- src/front/js/component/Rating/Hearts.jsx
+++ src/front/js/component/Rating/Hearts.jsx
@@ -1,20 +1,24 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import style from "../Rating/rating.module.css"
+import { FaRegHeart } from "react-icons/fa";
 
 
-export const Hearts = ({ pulsado, onClick }) => {
+export const Hearts = () => {
 
-    const heartClass = `${style.heart} ${pulsado ? style.pulsado : ''}`;
-    console.log(heartClass); // Verifica el nombre de la clas
+    // const heartClass = `${style.heart} ${pulsado ? style.pulsado : ''}`;
+    // console.log(heartClass); // Verifica el nombre de la clas
     
     return (
         <span
-            className={`${style.heart} ${pulsado ? style.pulsado : ''}`}
-            onClick={onClick}
+            className={`${style.heart}`}
+            // onClick={onClick}
         >
-            &#10084;
+            <FaRegHeart className=style.cor />
         </span>
 
     )
 }
+
+// Pulsado en false, on click pase a true
+// Pte rellenar
