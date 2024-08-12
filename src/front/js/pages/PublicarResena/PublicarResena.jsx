b3a61dbe2514c14abb21af5ac58052cd35f7f874
-TΦM- <137113975+JTomas88@users.noreply.github.com>
2024-08-12 19:50:02 +0000

    se sube jumbotron

--- src/front/js/pages/PublicarResena/PublicarResena.jsx
+++ src/front/js/pages/PublicarResena/PublicarResena.jsx
@@ -2,49 +2,41 @@ import React, { useContext, useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import style from "../PublicarResena/publicarresena.module.css"
 import { Rating } from "../../component/Rating/Rating.jsx";
+import { Jumbotron } from "../../component/Jumbotron/Jumbotron.jsx";
 
 export const PublicarResena = () => {
 
     return (
-        <div className={`${style.paginaLogin}`}>
-            <div className="container">
+        <>
+            <Jumbotron bgImg={{ backgroundImage: "url('https://images.pexels.com/photos/6939475/pexels-photo-6939475.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')" }} title={"¿Qué tal fue?"} subtitle={"Es importante que valores como ha sido el trabajo del acompañante."} />
+            <div className={`${style.paginaLogin}`}>
+                <div className="container">
 
-                <div className="row text-center mt-3 mb-5">
-                    <div>
-                        <h3>¿Qué tal fue?</h3>
+                    <div className="row mt-4">
+                        <Rating />
                     </div>
-                </div>
 
-                <div className="row text-center mt-3 mb-5">
-                    <div>
-                        <h4>Es importante que valores al acompañante: </h4>
+                    <div className="row justify-content-center d-flex">
+                        <div className="container text-center ">
+                            <p className={`text-start ${style.texto}`}>Observaciones</p>
+                            <textarea name="comentarios" cols={12} rows={8} className={`${style.observaciones}`} placeholder="Haznos saber cualquier cosa sobre el acompañante">
+
+                            </textarea >
+                        </div>
                     </div>
-                </div>
 
-                <div className="row">
-                    <Rating />
-                </div>
-
-                <div className="row justify-content-center d-flex">
-                    <div className="container text-center ">
-                        <p className={`text-start ${style.texto}`}>Observaciones</p>
-                        <textarea name="comentarios" cols={12} rows={8} className={`${style.observaciones}`}>
-
-                        </textarea >
+                    <div className="row">
+                        <div className="container mb-4 justify-content-center d-flex ">
+                            <button type="button" className={`btn btn-primary ${style.boton}`} >Enviar reseña</button>
+                        </div>
                     </div>
+
+
+
+
                 </div>
-
-                <div className="row">
-                    <div className="container mb-4 justify-content-center d-flex ">
-                        <button type="button" className={`btn btn-primary ${style.boton}`} >Enviar reseña</button>
-                    </div>
-                </div>
-
-
-
-
             </div>
-        </div>
+        </>
 
     )
 }
