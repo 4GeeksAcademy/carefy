a5f8d35d9bfe97cbad4a77dd3bf6a8708fbb63a5
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-08-13 13:29:48 +0000

    terminada vista crear anuncios

---
+++ src/front/js/pages/createAds/CreateAds.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './createAds.module.css'
+import DataAds from '../../component/dataAds/DataAds.jsx'
+
+
+const CreateAds = () => {
+  return (
+    <div className={styles.main_container}>
+        <DataAds/>
+      
+    </div>
+  )
+}
+
+export default CreateAds
