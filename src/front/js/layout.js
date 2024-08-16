b5a3ef6401332808aef98cab907829382c233973
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-08-16 06:06:20 +0000

    eliminado header

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
+
 import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+
+import ProfileCompanion from "./pages/profileCompanion/ProfileCompanion";
 
 
 //create your first component
@@ -24,16 +22,14 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
+               
                 
                     <Routes>
                         
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
+                        <Route element={<ProfileCompanion />} path="/perfil-profesional" />
+                       
                     </Routes>
-                    <Footer />
+                    
                 </ScrollToTop>
             </BrowserRouter>
         </div>
