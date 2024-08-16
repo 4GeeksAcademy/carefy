e476d63463cda25e9cf49c019051aa8df089959c
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-08-16 10:19:03 +0000

    añadidas lista de acompantes

--- src/front/js/component/filterCompanions/FilterCompanions.jsx
+++ src/front/js/component/filterCompanions/FilterCompanions.jsx
@@ -5,12 +5,7 @@ import styles from './filterCompanions.module.css'
 const FilterCompanions = () => {
   return (
     <div className={`container ${styles.filter_container}`}>
-      <div>
-        <h2>Conoce a Nuestros Expertos del Cuidado</h2>
-        <p>
-        Aquí podrás conocer a profesionales que están listos para brindar un cuidado atento y afectuoso. Estamos aquí para asegurarnos de que tus seres queridos reciban la atención cálida y dedicada que merecen.
-        </p>
-      </div>
+      
       <div className={`${styles.btn_filters} container-fluid text-center`}>
         <select className={`form-control ${styles.location}`}>
           <option>Ubicación</option>
--- src/front/js/pages/listCompanios/ListCompanions.jsx
+++ src/front/js/pages/listCompanios/ListCompanions.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 import CardsCompanions from '../../component/cardsCompanions/CardsCompanions.jsx'
 import FilterCompanions from '../../component/filterCompanions/FilterCompanions.jsx';
+import { Jumbotron } from '../../component/Jumbotron/Jumbotron.jsx';
 import styles from './listCompanions.module.css'
 
 const companions = [
+    {
+      "name": "Ana",
+      "last_name": "Martínez",
+      "location": "Madrid",
+      "ratings": 4.7,
+      "description": "Persona amable y paciente con experiencia previa en el cuidado de personas mayores. Ofrece apoyo y compañía durante el día.",
+      "photo": "https://randomuser.me/api/portraits/women/4.jpg"
+    },
     {
       "name": "Ana",
       "last_name": "Martínez",
@@ -37,7 +46,9 @@ const companions = [
 
 const ListCompanions = () => {
   return (
-    
+    <>
+        			<Jumbotron bgImg={ {backgroundImage: "url('https://media.istockphoto.com/id/1512266456/es/foto/una-anciana-con-la-enfermera-paseando-por-el-jard%C3%ADn-de-una-residencia-de-ancianos-en-silla-de.jpg?s=612x612&w=0&k=20&c=_7ABNTghwi1gB1IxvlLXSW0QcZofNSg88yo4bEK1pxM=')" }} title={"Conoce a nuestros expertos del cuidado"} subtitle={"Aquí podrás conocer a profesionales que están listos para brindar un cuidado atento y afectuoso."} />
+
     <div className={styles.container_main_profile}>
       
       <FilterCompanions />
@@ -59,7 +70,7 @@ const ListCompanions = () => {
         </div>
         </div>
         </div>
-    
+    </>
   )
 }
 
