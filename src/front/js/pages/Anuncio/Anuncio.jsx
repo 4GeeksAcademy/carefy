071c6a9a1f42e5424fff3c471137fd7b49128e8d
Javier Diez <javierdiez92@gmail.com>
2024-08-18 11:51:03 +0000

    FIX mostrar anuncios según estado

--- src/front/js/pages/Anuncio/Anuncio.jsx
+++ src/front/js/pages/Anuncio/Anuncio.jsx
@@ -1,18 +1,10 @@
 import React, { useContext, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import { Context } from "../../store/appContext.js";
 import { BloqueAnuncio } from "../../component/BloqueAnuncio/BloqueAnuncio.jsx";
 
 
 export const Anuncio = () => {
     const { store, actions } = useContext(Context);
-    const { id } = useParams();
-
-    useEffect(() => {
-        if (id) {
-            actions.getSingleAd(id);
-        }
-    }, [id]);
 
     return (
         <>
