e20e7a86511ca237cf26acbabb64311251994706
Javier Diez <javierdiez92@gmail.com>
2024-09-11 19:27:21 +0000

    ADD logo

--- webpack.common.js
+++ webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: 'favicon.png',
+        favicon: 'logo-carefy.png',
         template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
